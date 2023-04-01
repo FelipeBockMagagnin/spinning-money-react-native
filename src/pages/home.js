2a16b85dcd03d4468d4e28c096e4a84d9e136fcf
felipe.magagnin <felipebmagagnin@gmail.com>
2023-04-01 02:10:58 -0300

    Add player animation

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,24 @@
-import { StyleSheet, View, Text, Button } from 'react-native';
-import React, { useEffect } from 'react';
+import { StyleSheet, View, Text, Image, Button, Animated } from 'react-native';
+import React, { useEffect, useRef } from 'react';
 import Player from '../components/Player';
 import CurrencyManager from '../components/CurrencyManager';
+import MoneyImage from '../../assets/game_assets/money/Money_1.png';
 
 export default function HomeScreen() {
-    return (
-      <View style={styles.container}>
-        <CurrencyManager />
-        <Player/>
-      </View> 
-    );
-  }
 
-  const styles = StyleSheet.create({
-    container: {
-      position: 'absolute', 
-      top: 0, 
-      left: 0, 
-      right: 0, 
-      bottom: 0, 
-      justifyContent: 'center', 
-      alignItems: 'center'
-    }
-  }); 
+  return (
+    <View style={styles.container}>
+      <CurrencyManager />
+
+      <Player />
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  }
+}); 
